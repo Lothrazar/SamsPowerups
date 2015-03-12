951c18c0b4e06c932194782f80a14c750f7df7c7
Lothrazar <samson.bassett@gmail.com>
2015-03-12 00:52:02 -0700

    waterwalking potion complete

--- src/main/java/com/lothrazar/event/HandlerPotionTick.java
+++ src/main/java/com/lothrazar/event/HandlerPotionTick.java
@@ -4,6 +4,8 @@ import com.lothrazar.samscontent.ModLoader;
 import com.lothrazar.util.Reference;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,12 +13,11 @@ public class HandlerPotionTick
 { 
 	@SubscribeEvent
 	public void onEntityUpdate(LivingUpdateEvent event) 
-	{ 
-		 System.out.println("slowfallid_"+ModLoader.potionSlowfall.id);
-		     if(event.entityLiving.isPotionActive(ModLoader.potionTired)) 
-		     { 
-		    	if(event.entityLiving.worldObj.rand.nextInt(Reference.TICKS_PER_SEC) == 0) //pick out one random tick from each second
-		      {
+	{  
+	     if(event.entityLiving.isPotionActive(ModLoader.potionTired)) 
+	     { 
+	    	 if(event.entityLiving.worldObj.rand.nextInt(Reference.TICKS_PER_SEC) == 0) //pick out one random tick from each second
+	    	 {
 	        	 System.out.println("potionTired potion hit");
 	        	 
 	        	 //need drawbacks ... food cannot be full?
@@ -31,49 +32,44 @@ public class HandlerPotionTick
 	        		  
 	        		 if(player.getFoodStats().needFood() == false)//we are maxed out at 19 food
 	        		 {
-	        			 player.getFoodStats().addStats(-1, 0F);
+	        			 player.getFoodStats().addStats(-2, 0F);
 	        		 }
-	        	 }
-	        	 
-		      }
-	        	 
-	         } 
-		     if(event.entityLiving.isPotionActive(ModLoader.potionFlying)) 
-		     { 
-		    	 //TODO: importthe existing flying feature
-		     }
-		     if(event.entityLiving.isPotionActive(ModLoader.potionSlowfall)) 
-		     { 
-		    	 //a normal fall seems to go up to 0, -1.2, -1.4, -1.6, then flattens out at -0.078
-		    	 //https://github.com/OpenMods/OpenBlocks/blob/bfc6edd31e43982b434bcabcc21081c7e1fa6bc2/src/main/java/openblocks/common/entity/EntityHangGlider.java
-		    
-		    	 if(event.entityLiving.motionY < 0)
-		    	 {
-		    		 System.out.println("Reduce motion "+event.entityLiving.motionY);
-					  
-					event.entityLiving.motionY *= ModLoader.configSettings.slowfallSpeed;
-					  
-					event.entityLiving.fallDistance = 0f; //for no fall damage
-		    	 }
-					
-		     }
-		     if(event.entityLiving.isPotionActive(ModLoader.potionWaterwalk)) 
-		     { 
-		    	 
-		    	 //if the block at y-1 is water, and the block where i am is air, then
-		    	 //set motionY to zero, and somehow increase Movementfact/roWalking speed up increased
-		    	 //and set onGround = true.
-		    	 //Sneaking should give motionY -= 0.1F??
-	 
-		     }
-	     //}
-	     
-	     
-	     
-	     
-	     
-	     
-	     
+	        	 } 
+	    	  } 
+         } 
+	     if(event.entityLiving.isPotionActive(ModLoader.potionFlying)) 
+	     { 
+	    	 //TODO: importthe existing flying feature
+	    	 
+	    	 
+	    	 
+	     }
+	     if(event.entityLiving.isPotionActive(ModLoader.potionSlowfall)) 
+	     { 
+	    	 //a normal fall seems to go up to 0, -1.2, -1.4, -1.6, then flattens out at -0.078
+	    	 //https://github.com/OpenMods/OpenBlocks/blob/bfc6edd31e43982b434bcabcc21081c7e1fa6bc2/src/main/java/openblocks/common/entity/EntityHangGlider.java
+	    
+	    	 if(event.entityLiving.motionY < 0)
+	    	 { 
+				event.entityLiving.motionY *= ModLoader.configSettings.slowfallSpeed;
+				  
+				event.entityLiving.fallDistance = 0f; //for no fall damage
+	    	 } 
+	     }
+	     if(event.entityLiving.isPotionActive(ModLoader.potionWaterwalk)) 
+	     { 
+	    	 World world = event.entityLiving.worldObj;
+	    	 
+	    	 if(world.getBlockState(event.entityLiving.getPosition().down()).getBlock() == Blocks.water && 
+	    			 world.isAirBlock(event.entityLiving.getPosition()) && 
+	    			 event.entityLiving.motionY < 0)
+	    	 {
+	    		 event.entityLiving.motionY  = 0;//stop falling
+	    		 event.entityLiving.onGround = true; //act as if on solid ground
+	    		 event.entityLiving.setAIMoveSpeed(0.1F);//walking and not sprinting is this speed
+	    	 }  
+	     }
+  
          /*//TODO: do we even need this
          if (event.entityLiving.getActivePotionEffect(ModLoader.potionSlowfall).getDuration() == 0) 
          {
