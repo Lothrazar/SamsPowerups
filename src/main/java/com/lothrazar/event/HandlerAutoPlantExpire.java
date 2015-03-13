3869095aa7928ff5c450c8be6953dc44af50c374
Lothrazar <samson.bassett@gmail.com>
2015-03-12 17:26:30 -0700

    deny sapling growth based on matching biome to sapling tree type

--- src/main/java/com/lothrazar/event/HandlerAutoPlantExpire.java
+++ src/main/java/com/lothrazar/event/HandlerAutoPlantExpire.java
@@ -1,6 +1,7 @@
 package com.lothrazar.event;
 
 import com.lothrazar.samscontent.ModLoader;
+import com.lothrazar.util.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockState;
@@ -12,6 +13,7 @@ import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import net.minecraftforge.event.entity.player.PlayerDropsEvent;
 import net.minecraftforge.event.terraingen.SaplingGrowTreeEvent;
@@ -21,39 +23,104 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class HandlerAutoPlantExpire
 { 
 	
+	//for each biome, have list of saplings allowed
+	
 	@SubscribeEvent
 	public void onSaplingGrowTreeEvent(SaplingGrowTreeEvent event)
-	{
-		System.out.println("SaplingGrowTreeEvent ");
-		System.out.println(event.hasResult());
-
-		System.out.println("b    "+event.world.getBiomeGenForCoords(event.pos).biomeName
-				+"   "+event.isCancelable());
+	{ 
+		//IDEA: we could put biome/sapling pairs in config file and use 		BiomeGenBase.getBiome(int)
+		//AND OR: put field for saplings/biomes added by other mods
 		
-		//event.setResult(Result.DENY);
-		
+		BiomeGenBase biome = event.world.getBiomeGenForCoords(event.pos);
+		 
 		Block b = event.world.getBlockState(event.pos).getBlock();
 		
-		if(b == Blocks.sapling)
+		boolean treeAllowedToGrow = false;
+		
+		if(b == Blocks.sapling)//this may not always be true: such as trees added by Mods, so not a vanilla tree, but throwing same event
 		{
 			int meta = Blocks.sapling.getMetaFromState(event.world.getBlockState(event.pos));
 
 			int biomeID = event.world.getBiomeGenForCoords(event.pos).biomeID;
 			
-			System.out.println("GROW "+ b.getUnlocalizedName()+"   META   "+meta + " " 	+"   "+event.isCancelable());
+			int growth_data = 8;//0-5 is the type, then it adds on a 0x8  
+			//and we know that it is always maxed out at ready to grow 8 since it is turning into a tree.
 			
+			int tree_type = meta - growth_data;
 			
+			System.out.println(biome.biomeName + " tree_type   "+tree_type  );
 			
+			switch(tree_type)
+			{
+			case Reference.sapling_acacia:
+				
+				treeAllowedToGrow = (biome == BiomeGenBase.mesa)  || 
+					(biome == BiomeGenBase.savanna) ||
+					(biome == BiomeGenBase.savannaPlateau) || 
+					(biome == BiomeGenBase.mesaPlateau) || 
+					(biome == BiomeGenBase.mesaPlateau_F);
+		
+				break;
+			case Reference.sapling_spruce:
+
+				treeAllowedToGrow = (biome == BiomeGenBase.taiga) ||
+					(biome == BiomeGenBase.taigaHills) ||
+					(biome == BiomeGenBase.coldTaiga) ||
+					(biome == BiomeGenBase.coldTaigaHills) ||
+					(biome == BiomeGenBase.jungle) ||
+					(biome == BiomeGenBase.icePlains) ||
+					(biome == BiomeGenBase.iceMountains);
+				break; 
+			case Reference.sapling_oak:
+
+				treeAllowedToGrow = (biome == BiomeGenBase.forest) ||
+					(biome == BiomeGenBase.forestHills) ||
+					(biome == BiomeGenBase.jungle) ||
+					(biome == BiomeGenBase.jungleEdge) ||
+					(biome == BiomeGenBase.jungleHills) ||
+					(biome == BiomeGenBase.swampland);
+				break;
+			case Reference.sapling_birch:
+
+				treeAllowedToGrow = (biome == BiomeGenBase.birchForest)  || 
+					(biome == BiomeGenBase.birchForestHills) || 
+					(biome == BiomeGenBase.forest) ||
+					(biome == BiomeGenBase.forestHills);
+				
+				break;
+			case Reference.sapling_darkoak:
+
+				treeAllowedToGrow = (biome == BiomeGenBase.roofedForest);
+				break;
+			case Reference.sapling_jungle:
+
+				treeAllowedToGrow = (biome == BiomeGenBase.jungle) ||
+					(biome == BiomeGenBase.jungleEdge) ||
+					(biome == BiomeGenBase.jungleHills);
+				break;
+				
+				//no saplings for:
+				//frozen river
+				//ice plains spikes
+				//cold beach
+				//
+			}
+			 
+			if(treeAllowedToGrow == false)
+			{
+				event.setResult(Result.DENY);
+				System.out.println("treeAllowedToGrow DENY");
+				
+				//overwrite the sapling. - we could set to Air first, but dont see much reason to
+				event.world.setBlockState(event.pos, Blocks.deadbush.getDefaultState());
+			} 
 			
-			
-		}
+		}//else a tree grew that was added by some mod
 	}
 	
 	@SubscribeEvent
 	public void onItemExpireEvent(ItemExpireEvent event)
-	{ 
-		System.out.println("onItemExpireEvent " );
-		
+	{  
 		 if(ModLoader.configSettings.plantDespawningSaplings == false) {return;}
 		 
 		 ItemStack is = event.entityItem.getEntityItem();
