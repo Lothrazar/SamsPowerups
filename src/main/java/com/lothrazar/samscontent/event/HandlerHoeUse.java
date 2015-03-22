2eba5852af12ba0b5bb01dc83d6cae21993f664f
Lothrazar <samson.bassett@gmail.com>
2015-03-22 10:02:36 -0700

    beetroot seeds : require golden hoe

--- src/main/java/com/lothrazar/samscontent/event/HandlerHoeUse.java
+++ src/main/java/com/lothrazar/samscontent/event/HandlerHoeUse.java
@@ -5,6 +5,7 @@ import com.lothrazar.util.SamsUtilities;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -15,15 +16,15 @@ public class HandlerHoeUse
 	{ 
 		if(event.world.isRemote){return;}
 		if(event.world.isAirBlock(event.pos.up()) == false){return;}
-		//this fires BEFORE the block turns into farmland (is cancellable)
+		//this fires BEFORE the block turns into farmland (is cancellable) so check for grass and dirt, not farmland
 		
 		Block clicked = event.world.getBlockState(event.pos).getBlock();
-		//TODO: put the 1/16 odds in config file?
-		//TODO: restrict to only diamond or gold hoe? mabye also in config
-		if( clicked == Blocks.grass || clicked == Blocks.dirt )
-			if(event.world.rand.nextInt(16) == 0)
-			{					
-				SamsUtilities.dropItemStackInWorld(event.world, event.pos, ItemRegistry.beetrootSeed);
-			}
+		
+		if( (clicked == Blocks.grass || clicked == Blocks.dirt ) 
+			&& event.current.getItem() == Items.golden_hoe  //TODO:  maybe also in config
+			&& event.world.rand.nextInt(32) == 0)  //TODO: put the 1/x odds in config file?
+		{					
+			SamsUtilities.dropItemStackInWorld(event.world, event.pos, ItemRegistry.beetrootSeed);
+		}
 	}
 }
