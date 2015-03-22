2bfe8453e50f5deb6706745c9a2455ba173f8618
Lothrazar <samson.bassett@gmail.com>
2015-03-22 14:33:21 -0700

    achievements: one achievement for crafting each basic apple

--- src/main/java/com/lothrazar/samscontent/AchievementRegistry.java
+++ src/main/java/com/lothrazar/samscontent/AchievementRegistry.java
@@ -3,6 +3,7 @@ package com.lothrazar.samscontent;
 import com.lothrazar.samscontent.item.ItemRegistry;
 import com.lothrazar.util.Reference;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.stats.Achievement;
 import net.minecraft.stats.AchievementList;
@@ -13,22 +14,65 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemSmeltedEvent;
 
 public class AchievementRegistry 
 {
+	//reference http://www.minecraftforge.net/wiki/How_to_add_an_Achievement
+	 
 	private AchievementPage page;
 	
-	public Achievement test;
+	public Achievement appleChoc;
+	public Achievement appleLapis;
+	public Achievement appleDiamond;
+	public Achievement appleEmerald;
 	 
 	
 	public void registerAll()
 	{
-		//reference http://www.minecraftforge.net/wiki/How_to_add_an_Achievement
+		//Achievement constructor (id, name, x, y, icon, pre-requisite achievement 
 		
-		//TODO: lang file
-		test = new Achievement(Reference.MODID + "_appleChoc", "appleChoc", 1, -2, (Item)ItemRegistry.apple_chocolate, null);
-		test.registerStat(); //optional//.setSpecial()		 
+		int xSpacing = 3;
 		
-		page = new AchievementPage("Sam's Content",test);//, ach1, ach2, ach3, ach4
+		int xStart = 0;
+		int yStart = 0;
+		
+		int xCurrent = xStart;
+		int yCurrent = yStart;
+		
+		if(ItemRegistry.apple_chocolate != null)
+		{ 
+			xCurrent += xSpacing;
+			appleChoc = new Achievement(Reference.MODID + "_appleChoc", "appleChoc", xCurrent, yCurrent, ItemRegistry.apple_chocolate, null);
+			appleChoc.registerStat(); 
+		} 
+		if(ItemRegistry.apple_lapis != null)
+		{ 
+			xCurrent += xSpacing;
+			appleLapis = new Achievement(Reference.MODID + "_appleLapis", "appleLapis", xCurrent, yCurrent, ItemRegistry.apple_lapis, null);
+			appleLapis.registerStat(); 
+		} 
+		if(ItemRegistry.apple_diamond != null)
+		{ 
+			xCurrent += xSpacing;
+			appleDiamond = new Achievement(Reference.MODID + "_appleDiamond", "appleDiamond", xCurrent, yCurrent, ItemRegistry.apple_diamond, null);
+			appleDiamond.registerStat(); 
+		} 
+		if(ItemRegistry.apple_emerald != null)
+		{ 
+			xCurrent += xSpacing;
+			appleEmerald = new Achievement(Reference.MODID + "_appleEmerald", "appleEmerald", xCurrent, yCurrent, ItemRegistry.apple_emerald, null);
+			appleEmerald.registerStat(); 
+		} 
+ 
+
+		
+		//TODO: can page name be from lang file?
+		page = new AchievementPage("Sam's Content",appleChoc,appleDiamond,appleLapis,appleEmerald);//, ach1, ach2, ach3, ach4
   
 	 	AchievementPage.registerAchievementPage(page);
+	}
+	
+	private void addStatSafe(Achievement stat, EntityPlayer player)
+	{
+		if(stat != null) 
+			player.addStat(stat, 1);
 	}
 	
 	@SubscribeEvent
@@ -38,8 +82,19 @@ public class AchievementRegistry
 
 		if(event.crafting.getItem() == ItemRegistry.apple_chocolate)
 		{ 
-			event.player.addStat(test, 1);
+			addStatSafe(appleChoc,event.player);
 		}
-		//else if ... others
+		else if(appleLapis != null && event.crafting.getItem() == ItemRegistry.apple_lapis)
+		{ 
+			addStatSafe(appleLapis,event.player); 
+		}
+		else if(appleEmerald != null && event.crafting.getItem() == ItemRegistry.apple_emerald)
+		{ 
+			addStatSafe(appleEmerald,event.player); 
+		}
+		else if(appleDiamond != null && event.crafting.getItem() == ItemRegistry.apple_diamond)
+		{ 
+			addStatSafe(appleDiamond,event.player);  
+		}
 	}
 }
