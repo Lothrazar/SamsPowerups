c9b470eac9f424c1aec5385e8d21b9ca4c5b8990
Lothrazar <samson.bassett@gmail.com>
2015-04-01 19:37:20 -0700

    freeze potion packet: client side is getting hit now

--- src/main/java/com/lothrazar/samscontent/MessagePotion.java
+++ src/main/java/com/lothrazar/samscontent/MessagePotion.java
@@ -11,6 +11,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class MessagePotion implements IMessage, IMessageHandler<MessagePotion, IMessage>
 {
@@ -32,14 +33,28 @@ public class MessagePotion implements IMessage, IMessageHandler<MessagePotion, I
 	@Override
 	public IMessage onMessage(MessagePotion message, MessageContext ctx)
 	{ 
+		System.out.println("      ?? " );
+		if(ctx.side.isClient())// == Side.CLIENT
+		{
+			//  http://www.minecraftforge.net/forum/index.php?topic=21195.0
+			System.out.println("CLIENT  onMessage   " );
+			World world = Minecraft.getMinecraft().getIntegratedServer().getEntityWorld();
+			
+			SamsUtilities.spawnParticle(world, EnumParticleTypes.SNOWBALL, new BlockPos(0,5,0));
+			
+		}
+		else
+		{
+ 
+			System.out.println("SERVER  onMessage   " );	
+		}
+		/*
 		
-		World world = Minecraft.getMinecraft().getIntegratedServer().getEntityWorld();
 
-		SamsUtilities.spawnParticle(world, EnumParticleTypes.SNOWBALL, new BlockPos(0,5,0));
-	 
-			System.out.println("onMessage isRemote ==  "+world.isRemote);
+		
 	 
 System.out.println("got the message. client==null:   "+(ctx.getClientHandler() == null));
+/*
 /*
 EntityPlayer player = ctx.getServerHandler().playerEntity; 
 
