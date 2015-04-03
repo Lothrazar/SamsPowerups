3abcf515baf87f93d2217946d9269265f39f4f6b
Lothrazar <samson.bassett@gmail.com>
2015-04-02 21:39:28 -0700

    IExtended properties show using two different inteers from different places

--- src/main/java/com/lothrazar/samscontent/PlayerPowerups.java
+++ src/main/java/com/lothrazar/samscontent/PlayerPowerups.java
@@ -16,16 +16,16 @@ public class PlayerPowerups implements IExtendedEntityProperties
 //	private int currentFlyTicks = 0;
 	//private int currentHeartApplesEaten = 0;
 	public static final int SLEEP_WATCHER = 20;
-	private static final String NBT_SLEEP_CURRENT = "samSleeps";
+	private static final String NBT_SLEEP_CURRENT = "samNightBed";
 
+	public static final int AWAKE_WATCHER = 21;
+	private static final String NBT_AWAKE_CURRENT = "samNightUnslept"; 
 	public PlayerPowerups(EntityPlayer player)
 	{
-		this.player = player; 
-		int startFlyTicks = 0;
-		this.player.getDataWatcher().addObject(SLEEP_WATCHER, startFlyTicks);
+		this.player = player;  
+		this.player.getDataWatcher().addObject(SLEEP_WATCHER, 0);
+		this.player.getDataWatcher().addObject(AWAKE_WATCHER, 0);
 	}
-
- 
 	
 	public static final void register(EntityPlayer player)
 	{
@@ -36,12 +36,16 @@ public class PlayerPowerups implements IExtendedEntityProperties
 	{
 		return (PlayerPowerups) player.getExtendedProperties(EXT_PROP_NAME);
 	}
+	
+	
+	
 	@Override
 	public void saveNBTData(NBTTagCompound compound) 
 	{
 		// We need to create a new tag compound that will save everything for our Extended Properties
 		NBTTagCompound properties = new NBTTagCompound(); 
 		properties.setInteger(NBT_SLEEP_CURRENT, this.player.getDataWatcher().getWatchableObjectInt(SLEEP_WATCHER)); 
+		properties.setInteger(NBT_AWAKE_CURRENT, this.player.getDataWatcher().getWatchableObjectInt(AWAKE_WATCHER)); 
  
 		compound.setTag(EXT_PROP_NAME, properties); 
 	}
@@ -52,8 +56,9 @@ public class PlayerPowerups implements IExtendedEntityProperties
 		NBTTagCompound properties = (NBTTagCompound) compound.getTag(EXT_PROP_NAME);
 		// Get our data from the custom tag compound
 
-		this.player.getDataWatcher().updateObject(SLEEP_WATCHER, properties.getInteger(NBT_SLEEP_CURRENT));
-
+		//link each NBT property together with a watcher 
+		this.player.getDataWatcher().updateObject(SLEEP_WATCHER, properties.getInteger(NBT_SLEEP_CURRENT)); 
+		this.player.getDataWatcher().updateObject(AWAKE_WATCHER, properties.getInteger(NBT_AWAKE_CURRENT));
 		// Just so you know it's working, uncomment/ add this line:
 		//System.out.println("IEXTENDED T:currentFlyTicks " + this.player.getDataWatcher().getWatchableObjectInt(SLEEP_WATCHER)  );
 	}
@@ -62,17 +67,17 @@ public class PlayerPowerups implements IExtendedEntityProperties
 	public void init(Entity entity, World world) 
 	{ 
 	}
-	public final int getCurrentSleep()
+	public final int getInt(int WATCHER)//AWAKE_WATCHER
 	{
-		return this.player.getDataWatcher().getWatchableObjectInt(SLEEP_WATCHER);
+		return this.player.getDataWatcher().getWatchableObjectInt(WATCHER);
 	}
-	public final void incrementCurrentFly()
+	public final void increment(int WATCHER)
 	{ 
-		setCurrentSleep(getCurrentSleep() + 1);
+		setInt(WATCHER, getInt(WATCHER) + 1);
 	}
-	public final void setCurrentSleep(int amount)
+	public final void setInt(int WATCHER, int amount)
 	{
-		this.player.getDataWatcher().updateObject(SLEEP_WATCHER, amount);
+		this.player.getDataWatcher().updateObject(WATCHER, amount);
 	}
 	//http://www.minecraftforum.net/forums/mapping-and-modding/mapping-and-modding-tutorials/1571567-forge-1-6-4-1-8-eventhandler-and
 }
--- src/main/java/com/lothrazar/samscontent/event/PlayerSleep.java
+++ src/main/java/com/lothrazar/samscontent/event/PlayerSleep.java
@@ -2,6 +2,7 @@ package com.lothrazar.samscontent.event;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -10,7 +11,26 @@ import com.lothrazar.util.SamsUtilities;
 
 public class PlayerSleep 
 {
-	
+	@SubscribeEvent
+	public void onEntityUpdate(LivingUpdateEvent event) 
+	{  
+		if(event.entityLiving instanceof EntityPlayer)
+		{ 
+			EntityPlayer p = (EntityPlayer)event.entityLiving;
+
+//worldtime x => didnt sleep
+			
+			if(event.entity.worldObj.getWorldTime() == 13000 - 10)//opr it was 24000
+			{
+
+				PlayerPowerups props = PlayerPowerups.get(p);
+				props.increment(PlayerPowerups.AWAKE_WATCHER);
+	 			System.out.println("did not sleep the night,,.. "+props.getInt(PlayerPowerups.AWAKE_WATCHER));
+			}
+			
+			
+		}
+	}
 	@SubscribeEvent
  	public void onEntityConstructing(EntityConstructing event)
  	{ 
@@ -29,17 +49,20 @@ public class PlayerSleep
 		//.fires on both remotes
 		//wakeup early always false.
 	//	must check time of day for full night
+			//: wakeup  13000
 			System.out.println("wakeup  " +event.entity.worldObj.getWorldTime());
 			 
 			if(SamsUtilities.isNighttime(event.entity.worldObj))
-			{
-			//	SamsUtilities.incrementPlayerIntegerNBT(event.entityPlayer, "full_sleeps", 1);
-  
+			{ 
 			//	event.entityPlayer.getEntityData().setLong("last_sleep", event.entity.worldObj.getWorldTime());
 				PlayerPowerups props = PlayerPowerups.get(event.entityPlayer);
-				
-							props.incrementCurrentFly();
-				 			System.out.println("iextended properties "+props.getCurrentSleep());
+	
+				props.increment(PlayerPowerups.SLEEP_WATCHER);
+	 			System.out.println("iextended properties "+props.getInt(PlayerPowerups.SLEEP_WATCHER));
+	 			
+	 			
+	 			
+	 			
 				//TODO: this is unfinished. need to check current time
 				//and check number of days awake, and give tired buff
 				
