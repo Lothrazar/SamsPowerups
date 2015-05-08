9d13db083cb9a7c3c30491d74a98979d2ddb961a
sam bassett <samson.bassett@gmail.com>
2015-05-07 22:33:23 -0700

    version number updated

--- src/main/java/com/lothrazar/util/Reference.java
+++ src/main/java/com/lothrazar/util/Reference.java
@@ -13,7 +13,7 @@ public class Reference
 {  
 	public static final String MODID = "samscontent";
 	public static final String TEXTURE_LOCATION = Reference.MODID + ":";
-	public static final String VERSION = "1.8-1.1.2";
+	public static final String VERSION = "1.8-1.2.0";
 	public static final String NAME = "Builder's Powerups";
 
 	public static final String keyUpName = "key.columnshiftup";
