dd4ede2c2ada07558fded1befc915109eb8904c4
sam bassett <samson.bassett@gmail.com>
2015-06-02 15:59:07 -0700

    fixed all editor errors. old mod still has everything, as well as spell mod copied over to Spells dir

---
+++ Spells/src/main/java/com/lothrazar/samsmagic/ConfigRegistry.java
@@ -0,0 +1,89 @@
+package com.lothrazar.samsmagic;
+
+import net.minecraftforge.common.config.Configuration;  
+import com.lothrazar.samsmagic.entity.projectile.EntitySnowballBolt; 
+
+public class ConfigRegistry
+{ 
+	private Configuration instance;
+	private String category = "";
+	
+	public Configuration instance()
+	{
+		return instance;
+	}
+	
+	public ConfigRegistry(Configuration c)
+	{
+		instance = c;
+ 
+ 
+		category = "potion_effect_ids";
+
+		instance.addCustomCategoryComment(category, 
+				"IDs are only exposed to avoid conflicts with other mods.  Messing with these might break the game.   ");
+		
+		potionIdWaterwalk = instance.get(category,"waterwalk_id", 40).getInt();
+		  
+		potionIdSlowfall = instance.get(category,"slowfall_id", 41).getInt();
+		  
+		potionIdFlying = instance.get(category,"flying_id", 42).getInt();
+		
+		potionIdLavawalk = instance.get(category,"lavawalk_id", 43).getInt();
+ 
+		potionIdFrozen = instance.get(category,"frost_id", 44).getInt();
+		
+		potionIdEnder = instance.get(category,"ender_id", 45).getInt();
+		
+		category = "potion_effect_tweaks";
+		
+		slowfallSpeed = instance.getFloat("potion_slowfall_speed",category, 0.41F,0.1F,1F,
+    			"This factor affects how much the slowfall effect slows down the entity.");
+ 
+		appleChocolate = instance.get( category,"apple_chocolate",true).getBoolean();   
+		appleEmerald = instance.get( category,"apple_emerald",true).getBoolean();  
+		appleEnder = instance.get( category,"apple_ender",true).getBoolean();  
+		appleDiamond = instance.get(category, "apple_diamond",true).getBoolean(); 
+		appleNetherwart = instance.get(category, "apple_netherwart",true).getBoolean();
+		appleFrost = instance.get(category, "apple_frost",true).getBoolean(); 
+		
+		experience_bottle_return = instance.getBoolean("experience_bottle_return",category, true,
+				"Experience bottles in survival mode return an empty glass bottle to you (if used on a block).");  
+	
+		//TODO: Spell Configs
+		EntitySnowballBolt.secondsFrozenOnHit = instance.getInt("frozen_snowball.duration_on_hit",category, 25,1,600,
+    			"When something hit by one of these snowballs, it gets the snow effect for this many seconds.");
+		
+		//String csv = instance.getString("spell_piston.ignored",category, "minecraft:cactus,minecraft:piston_head,minecraft:piston_extension,minecraft:lit_furnace,minecraft:melon_stem,minecraft:pumpkin_stem,minecraft:wheat,samscontent:beetroot_crop,wooden_door,minecraft:spruce_door,minecraft:birch_door,minecraft:jungle_door,minecraft:acacia_door,minecraft:dark_oak_door,minecraft:iron_door,minecraft:bedrock,minecraft:tripwire,minecraft:tripwire_hook,minecraft:stone_button,minecraft:wooden_button,minecraft:stone_pressure_plate,minecraft:wooden_pressure_plate,minecraft:heavy_weighted_pressure_plate,minecraft:light_weighted_pressure_plate,minecraft:redstone_wire,minecraft:mob_spawner,minecraft:ladder",
+    	//		"List of ignored blocks that will not be moved.");
+		//UtilPistonSpell.seIgnoreBlocksFromString(csv);
+		
+		
+		
+		if(instance.hasChanged()){ instance.save(); }
+ 
+	}
+	public boolean experience_bottle_return;
+	 
+	public boolean appleDiamond;
+	//public boolean appleLapis;
+	public boolean appleChocolate;
+	public boolean appleEnder;
+
+	public boolean appleNetherwart;
+
+	public boolean appleEmerald;
+ 
+ 
+  
+	public int potionIdWaterwalk;
+	public int potionIdSlowfall;
+	public int potionIdFlying;
+	public float slowfallSpeed; 
+	public int potionIdLavawalk;
+	 
+	public int potionIdEnder;
+	public int potionIdFrozen; 
+	public int heartsCatTamed;
+	public boolean appleFrost;
+}
--- Spells/src/main/java/com/lothrazar/samsmagic/ModMain.java
+++ Spells/src/main/java/com/lothrazar/samsmagic/ModMain.java
@@ -1,10 +1,7 @@
 package com.lothrazar.samsmagic;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList; 
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;    
 
 import com.lothrazar.samsmagic.potion.*; 
@@ -13,96 +10,38 @@ import com.lothrazar.samsmagic.item.ItemChestSack;
 import com.lothrazar.samsmagic.proxy.*; 
 import com.lothrazar.samsmagic.spell.*; 
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockChest;
-import net.minecraft.block.BlockDoor;
-import net.minecraft.block.BlockFence;
-import net.minecraft.block.BlockFenceGate;
-import net.minecraft.block.BlockSandStone;
-import net.minecraft.block.BlockStairs;
-import net.minecraft.block.BlockStone;
-import net.minecraft.block.BlockStoneSlab;
-import net.minecraft.block.BlockTrapDoor;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderSnowball;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.passive.*; 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemDoor;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemSeeds;
-import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityChest;
-import net.minecraft.tileentity.TileEntityNote;
-import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
-import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
-import net.minecraftforge.event.entity.player.EntityInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent; 
-import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
-import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
-import net.minecraftforge.event.entity.player.UseHoeEvent;
-import net.minecraftforge.event.world.BlockEvent.BreakEvent;
-import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLInterModComms;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.common.registry.VillagerRegistry;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
   
 @Mod(modid = ModMain.MODID, version = ModMain.VERSION,	name = ModMain.NAME, useMetadata = true )  
@@ -117,7 +56,7 @@ public class ModMain
 	@SidedProxy(clientSide="com.lothrazar.samscontent.proxy.ClientProxy", serverSide="com.lothrazar.samscontent.proxy.CommonProxy")
 	public static CommonProxy proxy;   
 	public static Logger logger; 
-	//public static ConfigRegistry cfg;
+	public static ConfigRegistry cfg;
 	public static SimpleNetworkWrapper network; 
 	/*public static AchievementRegistry achievements;  
 	
@@ -135,7 +74,7 @@ public class ModMain
 	{ 
 		logger = event.getModLog();  
 		
-		//cfg = new ConfigRegistry(new Configuration(event.getSuggestedConfigurationFile()));
+		cfg = new ConfigRegistry(new Configuration(event.getSuggestedConfigurationFile()));
 	  
     	network = NetworkRegistry.INSTANCE.newSimpleChannel( MODID );     	
     	
@@ -197,57 +136,41 @@ public class ModMain
 	{ 
 		SpellSoulstone.onLivingHurt(event);
 	}
-	
+
+	public static EntityItem dropItemStackInWorld(World worldObj, BlockPos pos, ItemStack stack)
+	{
+		EntityItem entityItem = new EntityItem(worldObj, pos.getX(),pos.getY(),pos.getZ(), stack); 
+
+ 		if(worldObj.isRemote==false)//do not spawn a second 'ghost' one on client side
+ 			worldObj.spawnEntityInWorld(entityItem);
+    	return entityItem;
+	}
 	 
 	@SubscribeEvent
 	public void onPlayerInteract(PlayerInteractEvent event)
   	{        
 		ItemStack held = event.entityPlayer.getCurrentEquippedItem();
-		Block blockClicked = event.world.getBlockState(event.pos).getBlock(); 
-		TileEntity container = event.world.getTileEntity(event.pos);
+		//Block blockClicked = event.world.getBlockState(event.pos).getBlock(); 
+		//TileEntity container = event.world.getTileEntity(event.pos);
 
 		if(held != null && held.getItem() == Items.experience_bottle  && 
 				event.action.RIGHT_CLICK_BLOCK == event.action && 
 				event.entityPlayer.capabilities.isCreativeMode == false && 
 				ModMain.cfg.experience_bottle_return)
 		{ 
-			Util.dropItemStackInWorld(event.world, event.pos, Items.glass_bottle);
+			dropItemStackInWorld(event.world, event.pos, new ItemStack(Items.glass_bottle));
 		}
  
-	
 		if(held != null && held.getItem() == ItemRegistry.itemChestSack &&  //how to get this all into its own class
 				event.action.RIGHT_CLICK_BLOCK == event.action)
-		{ 
-	 
-				if(event.face != null && event.world.isAirBlock(event.pos.offset(event.face)) == false)
-				{
-					ItemChestSack.createAndFillChest(event.entityPlayer, held, event.pos.offset(event.face));
-				}
-		}
-	
-		
-	 
-		
-		 
-   	}
- 
-
-	@SubscribeEvent
-	public void onPlayerWakeUpEvent(PlayerWakeUpEvent event)
-	{
-		if(event.entityPlayer.worldObj.isRemote == false)
-		{
-			boolean didSleepAllNight = !event.updateWorld;
-			
-			if(didSleepAllNight && ModMain.cfg.sleeping_hunger_seconds > 0)
-			{ 
-				int levelBoost = 0;//1 means Hunger II. int = 2 means Hunger III, etc.
-				
-				event.entityPlayer.addPotionEffect(new PotionEffect(Potion.hunger.id,  ModMain.cfg.sleeping_hunger_seconds * Reference.TICKS_PER_SEC, levelBoost));
+		{  
+			if(event.face != null && event.world.isAirBlock(event.pos.offset(event.face)) == false)
+			{
+				ItemChestSack.createAndFillChest(event.entityPlayer, held, event.pos.offset(event.face));
 			}
-		}
-	}
-	
+		} 
+   	}
+  
 	@SubscribeEvent
 	public void onEnderTeleportEvent(EnderTeleportEvent event)
 	{  
