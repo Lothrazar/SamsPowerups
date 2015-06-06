58e7f3a976f96a54a3450f917b704b6df12035c2
sam bassett <samson.bassett@gmail.com>
2015-06-05 22:46:13 -0700

    nature: bonemeal event added back in

--- Nature/src/main/java/com/lothrazar/samsnature/ModNature.java
+++ Nature/src/main/java/com/lothrazar/samsnature/ModNature.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 import org.apache.logging.log4j.Logger;     
  
+
+
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
@@ -15,6 +19,7 @@ import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
@@ -59,7 +64,7 @@ public class ModNature
 		ArrayList<Object> handlers = new ArrayList<Object>();
 		  
       	handlers.add(new SaplingDespawnGrowth());//this is only one needs terrain gen buff, plus one of the regular ones
-      	handlers.add(instance                         );  
+      	handlers.add(instance                         );   
 
      	for(Object h : handlers)
      		if(h != null)
@@ -71,6 +76,80 @@ public class ModNature
 	     	} 
 		 
 		 
+	}
+	public static final int dye_bonemeal = 15;
+	public static boolean isBonemeal(ItemStack held )
+	{ 
+		Item heldItem = (held == null) ? null : held.getItem();
+		
+		if(heldItem == null){return false;}
+	 
+		return (heldItem.equals(Items.dye)  && held.getItemDamage() == dye_bonemeal); 
+	}
+	@SubscribeEvent
+	public void onPlayerInteract(PlayerInteractEvent event)
+  	{        
+		ItemStack held = event.entityPlayer.getCurrentEquippedItem();
+		Block blockClicked = event.world.getBlockState(event.pos).getBlock(); 
+		if(  event.action == event.action.RIGHT_CLICK_BLOCK && 
+  				isBonemeal(held)  && 
+  				blockClicked != null ) 
+		{    
+			useBonemeal(event.world, event.entityPlayer, event.pos, blockClicked);
+		}
+  	}
+	private void useBonemeal(World world, EntityPlayer entityPlayer, BlockPos pos, Block blockClicked)
+	{
+		boolean success = false; 
+		 
+  		//we could do a 1/3 odds chance or something, but tall flowers work everytime so following that
+  		
+  		if(ModNature.cfg.bonemealAllFlowers )
+  		{ 
+		 	if ( blockClicked.equals(Blocks.yellow_flower))//yellow flowers have no damage variations
+		 	{   
+		 		ModNature.dropItemStackInWorld(world, pos,new ItemStack(Blocks.yellow_flower));
+		
+			  	success = true;
+		 	}
+		 	else if ( blockClicked.equals(Blocks.red_flower)) 	//the red flower is ALL the flowers
+		 	{    
+		 		ModNature.dropItemStackInWorld(world, pos, new ItemStack(Blocks.red_flower ,1,Blocks.red_flower.getMetaFromState(world.getBlockState(pos))));
+		
+			  	success = true; 
+		 	} 
+  		}
+	 	
+	 	if(ModNature.cfg.bonemealLilypads && blockClicked.equals(Blocks.waterlily))
+	 	{ 
+	 		ModNature.dropItemStackInWorld(world, pos,new ItemStack(Blocks.waterlily));
+	 	 
+		  	success = true;
+	 	} 
+	 	if(ModNature.cfg.bonemealReeds && blockClicked.equals(Blocks.reeds))
+	 	{
+	 		//do not drop items, just grow them upwards
+	 	 
+	 		int goUp = 0;
+	 		
+	 		if(world.isAirBlock(pos.up(1))) goUp = 1;
+	 		else if(world.isAirBlock(pos.up(2))) goUp = 2;
+
+			if(goUp > 0)
+			{
+				world.setBlockState(pos.up(goUp), Blocks.reeds.getDefaultState());
+
+			  	success = true; 
+			} 
+	 	}
+	 	
+	 	if(success)
+	 	{ 
+	 		//the game also uses VILLAGER_HAPPY for their bonemeal events so i copy
+	 		ModNature.spawnParticle(world, EnumParticleTypes.VILLAGER_HAPPY, pos);
+	 		 
+	 		ModNature.decrHeldStackSize(entityPlayer); 
+	 	} 
 	}
 	@SubscribeEvent
 	public void onHoeUse(UseHoeEvent event)
