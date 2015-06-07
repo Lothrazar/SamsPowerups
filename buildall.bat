6206cb5697f50a8e6b5cdcc5afdbf9e5219ea013
sam bassett <samson.bassett@gmail.com>
2015-06-06 17:59:59 -0700

    new .bat scripts: to build all projects, and copy output to one place

---
+++ buildall.bat
@@ -0,0 +1,25 @@
+cd Apples
+START gradlew build
+cd ../Armor
+START gradlew build
+cd ../Blocks
+START gradlew build
+cd ../CarbonPaper
+START gradlew build
+cd ../HorseFood
+START gradlew build
+cd ../Macros
+START gradlew build
+cd ../MobChanges
+START gradlew build
+cd ../Nature
+START gradlew build
+cd ../Recipes
+START gradlew build
+cd ../Screentext
+START gradlew build
+cd ../Spells
+START gradlew build
+cd ../Stacks
+START gradlew build
+pause
---
+++ copyall.bat
@@ -0,0 +1,13 @@
+echo f | xcopy /f /y Apples\build\libs run
+echo f | xcopy /f /y Armor\build\libs run
+echo f | xcopy /f /y Blocks\build\libs run
+echo f | xcopy /f /y CarbonPaper\build\libs run
+echo f | xcopy /f /y HorseFood\build\libs run
+echo f | xcopy /f /y Macros\build\libs run
+echo f | xcopy /f /y MobChanges\build\libs run
+echo f | xcopy /f /y Nature\build\libs run
+echo f | xcopy /f /y Recipes\build\libs run
+echo f | xcopy /f /y Screentext\build\libs run
+echo f | xcopy /f /y Spells\build\libs run
+echo f | xcopy /f /y Stacks\build\libs run
+pause
