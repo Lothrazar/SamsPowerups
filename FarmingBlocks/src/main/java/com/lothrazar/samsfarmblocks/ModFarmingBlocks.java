2f083784e5676fd7a742dc33f0a7c9fbafd1903a
sam bassett <samson.bassett@gmail.com>
2015-07-08 15:56:36 -0700

    Food Details, tiny standalone mod

--- FarmingBlocks/src/main/java/com/lothrazar/samsfarmblocks/ModFarmingBlocks.java
+++ FarmingBlocks/src/main/java/com/lothrazar/samsfarmblocks/ModFarmingBlocks.java
@@ -3,12 +3,10 @@ package com.lothrazar.samsfarmblocks;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -16,11 +14,12 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ModFarmingBlocks.MODID, useMetadata=true)
 public class ModFarmingBlocks
 {
+    public static final String MODID = "samsfarmblocks";
+	public static final String TEXTURE_LOCATION = MODID + ":"; 
 	@Instance(value = ModFarmingBlocks.MODID)
 	public static ModFarmingBlocks instance;
 	public static CreativeTabs tabSFarming = new CreativeTabs("tabSFarming") 
@@ -34,8 +33,6 @@ public class ModFarmingBlocks
 
 	@SidedProxy(clientSide="com.lothrazar.samsfarmblocks.ClientProxy", serverSide="com.lothrazar.samsfarmblocks.CommonProxy")
 	public static CommonProxy proxy;  
-    public static final String MODID = "samsfarmblocks";
-	public static final String TEXTURE_LOCATION = MODID + ":"; 
 	
     @EventHandler
 	public void onPreInit(FMLPreInitializationEvent event)
@@ -46,24 +43,6 @@ public class ModFarmingBlocks
 		MinecraftForge.EVENT_BUS.register(instance); 
 	}
 
-    //TODO: to its own mod? stay here? ??
-    @SubscribeEvent
-    public void onItemTooltipEvent(ItemTooltipEvent event)
-    {
-    	//https://www.reddit.com/r/minecraftsuggestions/comments/3brh7v/when_hovering_over_a_food_it_shows_how_many_food/
-    	//right now shows regardless of flag event.showAdvancedItemTooltips
-    	//could change with config
-    	if(event.itemStack != null && event.itemStack.getItem() instanceof ItemFood)
-    	{
-    		ItemFood food = (ItemFood)event.itemStack.getItem();
-    		
-    		int hunger = food.getHealAmount(event.itemStack);
-    		float satur = food.getSaturationModifier(event.itemStack);
-    		 
-    		event.toolTip.add(hunger+" ("+satur+")");
-    	} 
-    }
-    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
