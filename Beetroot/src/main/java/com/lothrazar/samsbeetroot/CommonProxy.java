666a9b0107351e5ad2a187a590365250a82e4219
sam bassett <samson.bassett@gmail.com>
2015-08-07 07:18:07 -0700

    beetroot fork

--- Beetroot/src/main/java/com/lothrazar/samsbeetroot/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package com.lothrazar.samsbeetroot;
-
-public class CommonProxy 
-{   
-    public void registerRenderers() 
-    {
-            // Nothing here as the server doesn't render graphics or entities!
-    }
-}
